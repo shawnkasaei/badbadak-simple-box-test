74b0a041d95cfc2dc8478b2528538cb00b4122cf
Shawn Kasaei <shayankasaei@gmail.com>
2024-05-01 11:58:29 +0330

    Fixed Player.rotationObject not reseting when switching to Input System #2

--- Assets/Scripts/MovementSystem/AgentController.cs
+++ Assets/Scripts/MovementSystem/AgentController.cs
@@ -7,31 +7,32 @@ public class AgentController : MonoBehaviour
 
     private NavMeshAgent agent;
     private Camera camera;
+    private Transform rotationObj;
     
     private void Awake()
     {
         Instance = this;
         camera = Camera.main;
         agent = GetComponent<NavMeshAgent>();
+        rotationObj = transform.GetChild(0);
     }
-
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit; 
-            if(Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit))
             {
                 agent.SetDestination(hit.point);
             } 
         }
     }
-    
 
     public void Enable()
     {
+        rotationObj.eulerAngles = Vector3.zero;
         agent.enabled = true;
         enabled = true;
     }
